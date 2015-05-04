10d64819a4a01c82260eb562d9eb1124b9b0269d
DanielAndreasen <daniel.andreasen@astro.up.pt>
2015-05-04 15:37:15 +0100

    Structure for packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+setup(name='dinnerPlanner',
+      version='0.1',
+      description='Plan dinner for several days',
+      url='https://github.com/DanielAndreasen/Dinner-planner',
+      author='Daniel T. Andreasen',
+      author_email='daniel.andreasen@hotmail.com',
+      license='MIT',
+      packages=['dinnerPlanner'],
+      zip_safe=False)
